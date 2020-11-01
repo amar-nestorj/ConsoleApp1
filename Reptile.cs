6b35994b800db5113051cf852bc29e4a201b6164
Nestor T. Amar Jr <72591828+amar-nestorj@users.noreply.github.com>
2020-11-01 15:22:40 +0800

    Update Reptile.cs

--- Reptile.cs
+++ Reptile.cs
@@ -17,4 +17,44 @@ namespace prelim_exam
             Console.WriteLine(this._ofType);
         }
     }
+    
+    public class Snake : Reptile,Ireptile
+    {
+        public void snake()
+        {
+            this._numberOfLegs = 4;
+            this.Name = "Orochi";
+            this.CanSwim = true;
+
+            Console.WriteLine("Snake");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine(this.Name + "legs: " + this._numberOfLegs);
+            Console.WriteLine(this.Name + "can swim: " + this.CanSwim);
+        }
+
+        public void snakeInfo()
+        {
+            Snake snake = new Snake();
+            snake.snake();
+        }
+        
+    public class Croc : Reptile,Ireptile
+    {
+        public void croc()
+        {
+            this._numberOfLegs = 4;
+            this.Name = "government";
+            this.CanSwim = true;
+
+            Console.WriteLine("Crocodile");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine(this.Name + "legs: " + this._numberOfLegs);
+            Console.WriteLine(this.Name + "can swim: " + this.CanSwim);
+        }
+
+        public void crocInfo()
+        {
+            Croc croc = new Croc();
+            croc.croc();
+        }
 }
