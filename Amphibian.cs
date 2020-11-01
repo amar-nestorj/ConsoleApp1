89ce87c9d8fbc371e096a030eaad70f4e54ae5f5
Nestor T. Amar Jr <72591828+amar-nestorj@users.noreply.github.com>
2020-11-01 14:04:38 +0800

    Update Amphibian.cs

--- Amphibian.cs
+++ Amphibian.cs
@@ -4,14 +4,71 @@ namespace prelim_exam
 {
     public class Amphibian : Animal
     {
+        //Amphibians are cold-blooded, four-limbed vertebrates within the class Amphibia
+
+        public bool Coldblooded { get; set; }
+        
         public override string GetClass()
         {
-            throw new NotImplementedException();
+            this._class = "Amphibia";
+            Console.WriteLine(this._class);
         }
 
         public override string OfType()
         {
-            throw new NotImplementedException();
+            this._ofType = "Amphibian";
+            Console.WriteLine(this._ofType);
         }
+        
+    public class Frog : Amphibian,Iamphibian
+    {
+        public void frog()
+        {
+            Amphibian frog = new Amphibian();
+            frog.Coldblooded = true;
+            this._numberOfLegs = 4;
+            this.CanSwim = true;
+            this.Name = "Froggy";
+
+            Console.WriteLine("Frog");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine(this.Name + "legs: " + this._numberOfLegs);
+            Console.WriteLine(this.Name + "can swim: " + this.CanSwim);
+
+
+        }
+
+        public void froginfo()
+        {
+            Frog frog = new Frog();
+            frog.frog();
+        }
+    }   
+     
+        
+    public class Salamander : Amphibian,Iamphibian
+    {
+
+        public void salamander()
+        {
+            Amphibian salamander = new Amphibian();
+            salamander.Coldblooded = true;
+            this._numberOfLegs = 4;
+            this.CanSwim = true;
+            this.Name = "Newt";
+
+            Console.WriteLine("Salamander");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine(this.Name + "legs: " + this._numberOfLegs);
+            Console.WriteLine(this.Name + "can swim: " + this.CanSwim);
+        }
+        
+        public void salamanderinfo()
+        {
+            Salamander salamander = new Salamander();
+            salamander.salamander();
+        }
+     }
+    
     }
 }
