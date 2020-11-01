7014c4b59e8b7e4bcd685b5a1bd109935f2e1fd5
Nestor T. Amar Jr <72591828+amar-nestorj@users.noreply.github.com>
2020-11-01 14:37:39 +0800

    Update Fish.cs

--- Fish.cs
+++ Fish.cs
@@ -4,14 +4,65 @@ namespace prelim_exam
 {
     public class Fish : Animal
     {
-        public override string GetClass()
+        //Fishes are the aquatic animals within the class Anatidae that do not have limbs with digits.
+        public int Limbs { get; set; }
+
+        public override void GetClass()
         {
-            throw new NotImplementedException();
+            this._class = "Anatidae";
+            Console.WriteLine(this._class);
         }
 
-        public override string OfType()
+        public override void GetType()
         {
-            throw new NotImplementedException();
+            this._ofType = "Fish";
+            Console.WriteLine(this._ofType);
+        
+    }
+    
+    public class Clown : Fish,Ifish
+    {
+        public void clown()
+        {
+            Fish clown = new Fish();
+            this.Name = "Nemo";
+            this._numberOfLegs = 0;
+            this.CanSwim = true;
+
+            Console.WriteLine("Clown Fish");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine(this.Name + "Legs: " + _numberOfLegs);
+            Console.WriteLine(this.Name + "can swim: " + this.CanSwim);
+
+        }
+
+        public void clownInfo()
+        {
+            Clown clown = new Clown();
+            clown.clown();
+        }
+    }
+    
+    public class Bluetang : Fish,Ifish
+    {
+        public void bluetang()
+        {
+            Fish bluetang = new Fish();
+            this.Name = "Dory";
+            this._numberOfLegs = 0;
+            this.CanSwim = true;
+
+            Console.WriteLine("Blue Tang");
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine(this.Name + "Legs: " + _numberOfLegs);
+            Console.WriteLine(this.Name + "can swim: " + this.CanSwim);
+
+        }
+
+        public void bluetangInfo()
+        {
+            Bluetang bluetang = new Bluetang();
+            bluetang.bluetang();
         }
     }
 }
